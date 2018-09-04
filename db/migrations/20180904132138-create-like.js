25b62965eea1246b9cd9b3a6f55abfc8faf806ab
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 23:15:17 +0900

    feat(db/migration): Likesテーブルを作成するmigrationを追加

---
+++ db/migrations/20180904132138-create-like.js
@@ -0,0 +1,44 @@
+'use strict'; // eslint-disable-line
+module.exports = {
+  up: (queryInterface, Sequelize) => { // eslint-disable-line
+    return queryInterface.createTable('Likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+      },
+      itemId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Items',
+          key: 'id',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => { // eslint-disable-line
+    return queryInterface.dropTable('Likes', { force: true });
+  },
+};
