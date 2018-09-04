0020abba4ba1beb11c09066e848c2d15d427b4e3
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 23:16:37 +0900

    feat(model/like): Likeモデルを追加

---
+++ src/server/models/like.js
@@ -0,0 +1,39 @@
+'use strict'; // eslint-disable-line
+module.exports = (sequelize, DataTypes) => {
+  const Like = sequelize.define('Like', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    itemId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+  }, {});
+  Like.associate = function(models) { // eslint-disable-line
+    // associations can be defined here
+    Like.User = Like.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+    Like.Item = Like.belongsTo(models.Item, {
+      foreignKey: 'itemId',
+      as: 'item',
+    });
+  };
+  return Like;
+};
