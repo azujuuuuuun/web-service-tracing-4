96a57f9ec745eadd64612b9971cbf7c1cc0e25b5
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 23:16:14 +0900

    feat(db/seed): デモLikeを追加

---
+++ db/seeders/20180904141038-demo-like.js
@@ -0,0 +1,34 @@
+'use strict'; // eslint-disable-line
+
+module.exports = {
+  up: (queryInterface, Sequelize) => { // eslint-disable-line
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    const now = new Date();
+    return queryInterface.bulkInsert('Likes', [{
+      createdAt: now,
+      updatedAt: now,
+      userId: 2,
+      itemId: 1,
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => { // eslint-disable-line
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Likes', null, {});
+  },
+};
