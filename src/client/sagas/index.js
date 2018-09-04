2333e02ace031476de9b441ddba8fb8e9257bba7
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 23:21:33 +0900

    feat(saga/index): like Sagaを追加

--- src/client/sagas/index.js
+++ src/client/sagas/index.js
@@ -3,12 +3,14 @@ import { all, fork } from 'redux-saga/effects';
 import authSaga from './auth';
 import userSaga from './user';
 import itemSaga from './item';
+import likeSaga from './like';
 
 function* mySaga() {
   yield all([
     fork(authSaga),
     fork(userSaga),
     fork(itemSaga),
+    fork(likeSaga),
   ]);
 }
 
