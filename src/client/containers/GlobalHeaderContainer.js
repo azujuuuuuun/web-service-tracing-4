cf9825f8bfb9a999f6d42b7ebed15c1e098f8f18
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 17:46:24 +0900

    feat(container/GlobalHeaderContainer): GlobalHeaderContainerコンポーネントを追加

---
+++ src/client/containers/GlobalHeaderContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalHeader from '../components/GlobalHeader';
+
+class GlobalHeaderContainer extends React.Component { // eslint-disable-line
+  render() {
+    return (
+      <GlobalHeader />
+    );
+  }
+}
+
+export default GlobalHeaderContainer;
