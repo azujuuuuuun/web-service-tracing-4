d4ad2ba3d6a3cc64ed489dd9824e85e8875d8ad2
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 17:34:10 +0900

    feat(model/item): Itemモデルを追加

---
+++ src/server/models/item.js
@@ -0,0 +1,39 @@
+'use strict'; // eslint-disable-line
+module.exports = (sequelize, DataTypes) => {
+  const Item = sequelize.define('Item', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    body: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+  }, {});
+  Item.associate = function(models) { // eslint-disable-line
+    // associations can be defined here
+    Item.User = Item.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+  return Item;
+};
