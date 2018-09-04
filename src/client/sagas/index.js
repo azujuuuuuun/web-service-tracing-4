377842089a23ba5c38ae7a04ae648d07512b5fab
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 19:05:23 +0900

    feat(saga/index): user Sagaを追加

--- src/client/sagas/index.js
+++ src/client/sagas/index.js
@@ -1,11 +1,13 @@
 import { all, fork } from 'redux-saga/effects';
 
 import authSaga from './auth';
+import userSaga from './user';
 import itemSaga from './item';
 
 function* mySaga() {
   yield all([
     fork(authSaga),
+    fork(userSaga),
     fork(itemSaga),
   ]);
 }
