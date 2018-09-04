7cefb6cced22fd5ac4e847aba3ae57bd38532f23
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 19:06:12 +0900

    feat(reducer/index): user reducerを追加

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -3,11 +3,13 @@ import { reducer as formReducer } from 'redux-form';
 
 import loading from './loading';
 import viewer from './viewer';
+import user from './user';
 import { item, items } from './item';
 
 export default combineReducers({
   loading,
   viewer,
+  user,
   item,
   items,
   form: formReducer,
