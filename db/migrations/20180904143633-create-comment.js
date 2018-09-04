bf8d5f5f267dcd93320a5467483e6d4462bfb585
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-05 00:25:35 +0900

    feat(db/migration): Commentsテーブルを作成するmigrationを追加

---
+++ db/migrations/20180904143633-create-comment.js
@@ -0,0 +1,48 @@
+'use strict'; // eslint-disable-line
+module.exports = {
+  up: (queryInterface, Sequelize) => { // eslint-disable-line
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      text: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+      },
+      itemId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Items',
+          key: 'id',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => { // eslint-disable-line
+    return queryInterface.dropTable('Comments', { force: true });
+  },
+};
