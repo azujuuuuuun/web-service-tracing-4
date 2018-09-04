12a1ce430b2a7d084c14d0052ebb8a3444fcd3d3
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 17:44:44 +0900

    feat(reducer/loading): fetchItem actionとfetchItems actionがdispatchされた時のstate更新処理を追加

--- src/client/reducers/loading.js
+++ src/client/reducers/loading.js
@@ -1,6 +1,10 @@
 import { createReducer } from 'redux-act';
 
-import { authenticateRequested, authenticateSucceeded, authenticateFailed } from '../actions';
+import {
+  authenticateRequested, authenticateSucceeded, authenticateFailed,
+  fetchItemRequested, fetchItemSucceeded, fetchItemFailed,
+  fetchItemsRequested, fetchItemsSucceeded, fetchItemsFailed,
+} from '../actions';
 
 const defaultState = true;
 
@@ -8,6 +12,12 @@ const loading = createReducer({
   [authenticateRequested]: () => true,
   [authenticateSucceeded]: () => false,
   [authenticateFailed]: () => false,
+  [fetchItemRequested]: () => true,
+  [fetchItemSucceeded]: () => false,
+  [fetchItemFailed]: () => false,
+  [fetchItemsRequested]: () => true,
+  [fetchItemsSucceeded]: () => false,
+  [fetchItemsFailed]: () => false,
 }, defaultState);
 
 export default loading;
