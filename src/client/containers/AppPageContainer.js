820de45f8ae56d7c4e68be18e4e76d9e05d77129
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 17:51:34 +0900

    feat(container/AppPageContainer): item一覧を取得する処理を追加

--- src/client/containers/AppPageContainer.js
+++ src/client/containers/AppPageContainer.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Loading from './LoadingContainer';
 import AppPage from '../components/AppPage';
+import { fetchItemsRequested } from '../actions';
 
 class AppPageContainer extends React.Component { // eslint-disable-line
+  componentDidMount() {
+    const { fetchItemsRequest } = this.props;
+    fetchItemsRequest();
+  }
+
   render() {
+    const { items } = this.props;
     return (
       <Loading>
-        <AppPage />
+        <AppPage items={items} />
       </Loading>
     );
   }
 }
 
-export default AppPageContainer;
+const mapStateToProps = state => ({
+  items: state.items,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchItemsRequest: () => dispatch(fetchItemsRequested()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AppPageContainer);
