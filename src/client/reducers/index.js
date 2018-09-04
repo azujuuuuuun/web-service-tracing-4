446f3dda8e286214c9d5235fb95ccdd927fbb249
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-04 17:43:52 +0900

    feat(reducer/index): item reducerとitems reducerを追加

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -3,9 +3,12 @@ import { reducer as formReducer } from 'redux-form';
 
 import loading from './loading';
 import viewer from './viewer';
+import { item, items } from './item';
 
 export default combineReducers({
   loading,
   viewer,
+  item,
+  items,
   form: formReducer,
 });
