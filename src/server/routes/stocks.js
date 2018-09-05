231dddab9d5b63b8944ba69541043a89d077997d
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-05 17:37:49 +0900

    feat(route/stocks): stocks routerを追加

---
+++ src/server/routes/stocks.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const db = require('../models');
+
+const router = express.Router();
+
+const { User, Stock } = db;
+
+router.post('/', async (req, res) => {
+  const { token } = req.headers;
+  if (!token) {
+    res.status(400).send('Token was undefined');
+  } else {
+    try {
+      const decoded = jwt.verify(token, 'shhhhh');
+      const { userId } = decoded;
+      const user = await User.findById(userId);
+      if (!user) {
+        res.status(400).send('User was not found');
+      } else {
+        const { itemId } = req.body;
+        const stock = await Stock.create({ userId, itemId });
+        res.status(200).send({ stock });
+      }
+    } catch (err) {
+      console.log(err); // eslint-disable-line no-console
+      res.status(400).send(err);
+    }
+  }
+});
+
+router.delete('/:itemId', async (req, res) => {
+  const { token } = req.headers;
+  if (!token) {
+    res.status(400).send('Token was undefined');
+  } else {
+    try {
+      const decoded = jwt.verify(token, 'shhhhh');
+      const { userId } = decoded;
+      const user = await User.findById(userId);
+      if (!user) {
+        res.status(400).send('User was not found');
+      } else {
+        const { itemId } = req.params;
+        await Stock.destroy({ where: { userId, itemId } });
+        res.sendStatus(204);
+      }
+    } catch (err) {
+      console.log(err); // eslint-disable-line no-console
+      res.status(400).send(err);
+    }
+  }
+});
+
+module.exports = router;
