8aad56abe8617466916b7762a3c1606e3d3e1818
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-05 16:15:36 +0900

    feat(component/UserPage): フォローボタンを追加

--- src/client/components/UserPage.js
+++ src/client/components/UserPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
 import GlobalHeader from '../containers/GlobalHeaderContainer';
 
 const UserPage = (props) => {
-  const { user } = props;
+  const {
+    user, isViewer, hasFollowed, followRequest, unfollowRequest,
+  } = props;
   return (
     <div>
       <GlobalHeader />
@@ -14,6 +17,25 @@ const UserPage = (props) => {
       ) : (
         <div>
           <h3>{`@${user.username}`}</h3>
+          {!isViewer && (
+            <div>
+              <Button
+                onClick={() => followRequest(user)}
+                disabled={hasFollowed}
+              >
+                フォロー
+              </Button>
+              <Button disabled={!hasFollowed}>
+                フォロー中
+              </Button>
+              <Button
+                onClick={() => unfollowRequest(user.id)}
+                disabled={!hasFollowed}
+              >
+                解除
+              </Button>
+            </div>
+          )}
           <div>
             {user.items.map(i => (
               <div key={i.id}>
