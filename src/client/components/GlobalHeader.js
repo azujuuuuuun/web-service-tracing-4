57d382d93a273a2cacbef936e0e62fe6034408ed
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-05 19:44:26 +0900

    feat(component/GlobalHeader): コミュニティドロップダウンを追加

--- src/client/components/GlobalHeader.js
+++ src/client/components/GlobalHeader.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import PeopleIcon from '@material-ui/icons/People';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
@@ -10,23 +11,41 @@ const GlobalHeader = (props) => {
   const {
     viewer,
     dropdown,
+    openCommunityDropdown,
     openViewerDropdown,
-    closeViewerDropdown,
+    closeDropdown,
   } = props;
   return (
     <div>
       <Link to="/">Qiita</Link>
+      <span>コミュニティ</span>
+      <IconButton onClick={openCommunityDropdown}>
+        <ArrowDropDownIcon />
+      </IconButton>
+      <Menu
+        open={dropdown.isOpen && dropdown.kind === 'community'}
+        onClose={closeDropdown}
+      >
+        <MenuItem>
+          <Link to="/users" onClick={closeDropdown}>
+            <PeopleIcon />
+            <span>ユーザー一覧</span>
+          </Link>
+        </MenuItem>
+      </Menu>
       <Link to="/drafts/new">投稿する</Link>
       <Avatar>{viewer.username}</Avatar>
       <IconButton onClick={openViewerDropdown}>
         <ArrowDropDownIcon />
       </IconButton>
       <Menu
-        open={dropdown.isOpen}
-        onClose={closeViewerDropdown}
+        open={dropdown.isOpen && dropdown.kind === 'viewer'}
+        onClose={closeDropdown}
       >
         <MenuItem>
-          <Link to={`/${viewer.username}`}>マイページ</Link>
+          <Link to={`/${viewer.username}`} onClick={closeDropdown}>
+            マイページ
+          </Link>
         </MenuItem>
       </Menu>
     </div>
