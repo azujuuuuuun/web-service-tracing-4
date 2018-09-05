9e628c0cca955c4c1ebcaa645b4d828476706528
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-05 19:42:40 +0900

    feat(component/UsersPage): UsersPageコンポーネントを追加

---
+++ src/client/components/UsersPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PeopleIcon from '@material-ui/icons/People';
+import { Link } from 'react-router-dom';
+
+import GlobalHeader from '../containers/GlobalHeaderContainer';
+
+const UsersPage = (props) => {
+  const { users } = props;
+  return (
+    <div>
+      <GlobalHeader />
+      <div>
+        <h2>
+          <PeopleIcon />
+          <span>ユーザー一覧</span>
+        </h2>
+        <p>Qiitaに登録しているユーザーの一覧です。 現在1万人以上のユーザーが登録しています。</p>
+      </div>
+      <div>
+        {users.map(u => (
+          <div key={u.id}>
+            <div>
+              <Link to={`/${u.username}`}>{u.username}</Link>
+            </div>
+            <p>{u.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default UsersPage;
