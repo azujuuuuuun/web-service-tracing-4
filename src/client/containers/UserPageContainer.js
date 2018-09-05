f282ebe61d9471f26981e8de0b0edad7da000dad
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-05 16:16:16 +0900

    feat(container/UserPageContainer): フォロー周りの処理を追加

--- src/client/containers/UserPageContainer.js
+++ src/client/containers/UserPageContainer.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 
 import Loading from './LoadingContainer';
 import UserPage from '../components/UserPage';
-import { fetchUserRequested } from '../actions';
+import {
+  fetchUserRequested, followRequested, unfollowRequested,
+} from '../actions';
 
 class UserPageContainer extends React.Component { // eslint-disable-line
   componentDidMount() {
@@ -13,21 +15,34 @@ class UserPageContainer extends React.Component { // eslint-disable-line
   }
 
   render() {
-    const { user } = this.props;
+    const {
+      viewer, user, followRequest, unfollowRequest,
+    } = this.props;
+    const isViewer = viewer.id === user.id;
+    const hasFollowed = viewer.followings.some(f => f.id === user.id);
     return (
       <Loading>
-        <UserPage user={user} />
+        <UserPage
+          user={user}
+          isViewer={isViewer}
+          hasFollowed={hasFollowed}
+          followRequest={followRequest}
+          unfollowRequest={unfollowRequest}
+        />
       </Loading>
     );
   }
 }
 
 const mapStateToProps = state => ({
+  viewer: state.viewer,
   user: state.user,
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchUserRequest: username => dispatch(fetchUserRequested({ username })),
+  followRequest: user => dispatch(followRequested({ user })),
+  unfollowRequest: followedId => dispatch(unfollowRequested({ followedId })),
 });
 
 export default connect(
