35bb160ab68a88f84224caa783d060b9b4ca5a0c
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-05 18:41:03 +0900

    feat(saga/like): unlike actionに対するSagaの処理を追加

--- src/client/sagas/like.js
+++ src/client/sagas/like.js
@@ -7,7 +7,10 @@ import {
   takeEvery,
 } from 'redux-saga/effects';
 
-import { likeRequested, likeSucceeded, likeFailed } from '../actions';
+import {
+  likeRequested, likeSucceeded, likeFailed,
+  unlikeRequested, unlikeSucceeded, unlikeFailed,
+} from '../actions';
 
 const Api = {
   like: async (itemId) => {
@@ -33,6 +36,25 @@ const Api = {
       return { err };
     }
   },
+  unlike: async (itemId) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return { err: 'Token was not found.' };
+    }
+    try {
+      await axios({
+        method: 'delete',
+        url: `/likes/${itemId}`,
+        headers: {
+          token,
+        },
+      });
+      return { data: 'Deleteing like succeeded' };
+    } catch (err) {
+      console.log(err); // eslint-disable-line no-console
+      return { err };
+    }
+  },
 };
 
 function* like(action) {
@@ -48,12 +70,33 @@ function* like(action) {
   }
 }
 
+function* unlike(action) {
+  try {
+    const { itemId, userId } = action.payload;
+    const { err, data } = yield call(Api.unlike, itemId);
+    if (err) {
+      yield put(unlikeFailed({ message: err.message }));
+    } else if (data) {
+      yield put(unlikeSucceeded({ itemId, userId }));
+    }
+  } catch (e) {
+    yield put(unlikeFailed({ message: e.message }));
+  }
+}
+
 function* watchLike() {
   yield takeEvery(likeRequested.getType(), like);
 }
 
+function* watchUnlike() {
+  yield takeEvery(unlikeRequested.getType(), unlike);
+}
+
 function* rootSaga() {
-  yield all([fork(watchLike)]);
+  yield all([
+    fork(watchLike),
+    fork(watchUnlike),
+  ]);
 }
 
 export default rootSaga;
