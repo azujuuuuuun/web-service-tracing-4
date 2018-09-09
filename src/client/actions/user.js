0d329bd495224bf4a4cf10d253a181747e8ac563
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-09 20:23:58 +0900

    feat(action/user): updatePassword actionを追加

--- src/client/actions/user.js
+++ src/client/actions/user.js
@@ -13,3 +13,7 @@ export const fetchUsersFailed = createAction('FETCH_USERS_FAILED');
 export const updateUserRequested = createAction('UPDATE_USER_REQUESTED');
 export const updateUserSucceeded = createAction('UPDATE_USER_SUCCEEDED');
 export const updateUserFailed = createAction('UPDATE_USER_FAILED');
+
+export const updatePasswordRequested = createAction('UPDATE_PASSWORD_REQUESTED');
+export const updatePasswordSucceeded = createAction('UPDATE_PASSWORD_SUCCEEDED');
+export const updatePasswordFailed = createAction('UPDATE_PASSWORD_FAILED');
