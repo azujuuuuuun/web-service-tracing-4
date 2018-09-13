ebd73c98c4b078010326e87a826110b8707ccf11
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-13 22:47:14 +0900

    feat(reducer/viewer): uploadImageSucceeded actionがdispatchされた時、avatarImgSrcを更新

--- src/client/reducers/viewer.js
+++ src/client/reducers/viewer.js
@@ -14,6 +14,7 @@ import {
   followTagSucceeded,
   unfollowTagSucceeded,
   updateNotificationsSucceeded,
+  uploadImageSucceeded,
 } from '../actions';
 
 const defaultState = {
@@ -77,6 +78,9 @@ const viewer = createReducer({
   }),
   [updateNotificationsSucceeded]: (state, payload) => Object.assign({}, state, {
     notifications: payload.notifications,
+  }),
+  [uploadImageSucceeded]: (state, payload) => Object.assign({}, state, {
+    avatarImgSrc: payload.avatarImgSrc,
   }),
 }, defaultState);
 
